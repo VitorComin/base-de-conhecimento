3d6bc9807a13168eff89f91c77f18a3304d859f6
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-06-04 21:20:03 -0300

    starting layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Input, Layout, Space, Typography } from "antd";
+import { EditOutlined, EllipsisOutlined } from "@ant-design/icons";
+import "./App.css";
+import { folderKnowledges, knowledgeFolders } from "./utils/config";
+import { useState } from "react";
+
+const { Header } = Layout;
 
 function App() {
+  const [folders, setFolders] = useState(knowledgeFolders);
+  const [knowledges, setKnowledges] = useState(folderKnowledges);
+  const [view, setView] = useState("folder");
+
+  function search(value: any) {
+    setFolders(
+      knowledgeFolders.filter((folder) =>
+        folder.title.includes(value.target.value)
+      )
+    );
+
+    setKnowledges(
+      folderKnowledges.filter((knowledge) =>
+        knowledge.title.includes(value.target.value)
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: "10vh",
+        }}
+      >
+        <Typography.Title
+          style={{
+            color: "white",
+            margin: 0,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Knowledges
+        </Typography.Title>
+        <Input style={{ width: 200, height: 40 }} onChange={search} />
+      </Header>
+      <Space
+        style={{
+          width: "100vw",
+          height: "90vh",
+          overflowX: "auto",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignContent: "start",
+          display: "grid",
+        }}
+      >
+        {view === "folder" ? (
+          folders.map((folder, index) => (
+            <Card
+              title={folder.title}
+              variant="borderless"
+              actions={[
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined
+                  key="ellipsis"
+                  onClick={() => setView("knowledge")}
+                />,
+              ]}
+              style={{ width: "80vw", marginTop: index === 0 ? 50 : 10 }}
+            >
+              <Typography.Paragraph ellipsis={{ rows: 1 }}>
+                {folder.description}
+              </Typography.Paragraph>
+            </Card>
+          ))
+        ) : view === "knowledge" ? (
+          knowledges.map((knowledge, index) => (
+            <Card
+              title={knowledge.title + " + " + knowledge.author}
+              variant="borderless"
+              actions={[
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined
+                  key="ellipsis"
+                  onClick={() => setView("show")}
+                />,
+              ]}
+              style={{ width: "80vw", marginTop: index === 0 ? 50 : 10 }}
+            >
+              <Typography.Paragraph ellipsis={{ rows: 1 }}>
+                {knowledge.description}
+              </Typography.Paragraph>
+            </Card>
+          ))
+        ) : (
+          <Card
+            title={"Conhecimento - Gabriel"}
+            variant="borderless"
+            style={{ width: "80vw", marginTop: 50 }}
+          >
+            <Typography.Paragraph>
+              {
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!"
+              }
+            </Typography.Paragraph>
+          </Card>
+        )}
+      </Space>
+    </>
   );
 }
 
---
+++ src/utils/config.tsx
@@ -0,0 +1,49 @@
+export const knowledgeFolders = [
+  {
+    title: "Pasta de conhecimento 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+  },
+  {
+    title: "Pasta de conhecimento 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+  },
+  {
+    title: "Pasta de conhecimento 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+  },
+  {
+    title: "Pasta de conhecimento 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+  },
+];
+
+export const folderKnowledges = [
+  {
+    title: "Conhecimento 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+    author: "Gabriel",
+  },
+  {
+    title: "Conhecimento 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+    author: "Gabriel",
+  },
+  {
+    title: "Conhecimento 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+    author: "Gabriel",
+  },
+  {
+    title: "Conhecimento 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur animi ab expedita! Eaque, culpa eos earum vero fuga recusandae nesciunt tenetur dolor illum quod praesentium commodi, facere mollitia soluta nobis!",
+    author: "Gabriel",
+  },
+];
